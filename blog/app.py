e0cea4e2bbb71690e9852ac077a128265df10893
Sergey Pestrikov <sergey_pestrikov@mail.ru>
2023-04-15 22:43:26 +0300

    added blueprints for User and Article + connected bootstrap with jinja templates

--- blog/app.py
+++ blog/app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 
-# Создание экземпляра приложения
-app = Flask(__name__)
+# Фабрика по созданию приложений
+from blog.user.views import user
+from blog.articles.views import article
+from blog.home.views import home
 
 
-@app.route('/')
-def index():
-    return 'The main page!'
+# Точка входа в приложение. Создание экземпляра приложения
+def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    # Который возвращает экземпляр приложения
+    return app
+
+
+# Регистрация блюпринта в приложении
+def register_blueprints(app: Flask):
+    app.register_blueprint(user)
+    app.register_blueprint(article)
+    app.register_blueprint(home)
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
-from blog.app import app
+from blog.app import create_app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(
         host='0.0.0.0',
-        port='5050',
-        debug=True,
+        port=5050,
+        debug=True
     )
