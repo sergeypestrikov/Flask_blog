2d310acf70843f6fb8cad6a259440067900237a8
Sergey Pestrikov <sergey_pestrikov@mail.ru>
2023-05-21 12:00:47 +0300

    Flask admin added

--- blog/extensions.py
+++ blog/extensions.py
@@ -2,8 +2,16 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
+from flask_admin import Admin
+
+from blog.admin.views import CustomAdminIndexView
 
 login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
+admin = Admin(
+    index_view=CustomAdminIndexView(),
+    name='Blog Admin Panel',
+    template_mode='bootstrap4',
+)
